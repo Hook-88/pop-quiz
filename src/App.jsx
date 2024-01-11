97af249d1026c84966878ff3e7944af76da12643
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-01-11 13:18:38 -0500

    Add logic to start quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import LandingPage from './components/LandingPage'
-import './App.css'
+import {useState, createContext} from "react"
+import LandingPage from "./components/LandingPage"
+import "./App.css"
 
+const QuizContext = createContext()
 
 function App() {
+  const [quizStarted, setQuizStarted] = useState(false)
+
+  function startQuiz() {
+    setQuizStarted(true)
+  }
+
 
   return (
     <div>
-      <LandingPage />
+      <QuizContext.Provider value={{quizStarted, startQuiz}}>
+      {!quizStarted ? <LandingPage /> : "quiz"}
+      </QuizContext.Provider>
     </div>
   )
 }
 
 export default App
+
+export { QuizContext }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,13 +1,15 @@
-import React from "react"
-import Button from "./Button"
+import {useContext} from "react"
+import Button from "./Button/Button"
+import { QuizContext } from "../App"
 
 export default function LandingPage() {
+  const {startQuiz} = useContext(QuizContext)
 
   return (
     <section className="card landing--page">
       <h1>Paulny's Pop quiz</h1>
       <p>A short quiz of five multiple choice questions</p>
-      <Button className="danger">Start the quiz</Button>
+      <Button onClick={startQuiz}>Start the quiz</Button>
     </section>
   )
   
