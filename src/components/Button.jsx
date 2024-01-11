21b3047dee4bdc0c75358a7e5e0c0d3d407a5590
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-01-11 13:07:07 -0500

    moved css of button to seperate file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import classNames from "classnames"
+import "./Button.css"
 
 export default function Button({children, className, ...rest}) {
   const btnClassname = classNames("button--normal", className ? className : "")
