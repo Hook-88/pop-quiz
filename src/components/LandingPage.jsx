a39e355d662c8a266234323884166c6adab88bf5
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-01-11 12:44:22 -0500

    improved styles

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function LandingPage() {
+
+  return (
+    <section className="card landing--page">
+      <h1>Paulny's Pop quiz</h1>
+      <p>A short quiz of five multiple choice questions</p>
+      <button className="button--normal">Start the Quiz</button>
+    </section>
+  )
+  
+}
+
