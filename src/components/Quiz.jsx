f9525b9832ed52245421a60c53f250e5cac7aca6
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-01-12 20:49:05 -0500

    add fetch request

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react"
+
+const URL = "https://opentdb.com/api.php?amount=5&type=multiple"
+
 export default function Quiz() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL)
+        const result = await response.json()
+        setData(result)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    fetchData()
+  }, [])
   
   return (
     <form>
