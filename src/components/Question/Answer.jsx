dbf34e2796f3fc52ade6636270d2f1406a627627
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-01-12 22:16:14 -0500

    add logic to set check on clicking span

--- src/components/Question/Answer.jsx
+++ src/components/Question/Answer.jsx
@@ -1,12 +1,25 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { QuestionContext } from "./Question"
 
 export default function Answer({children}) {
   const {id} = useContext(QuestionContext).questionData
+  const [on, setOn] = useState(false)
+
+  function select() {
+    setOn(true)
+  }
 
   return (
-    <span>
-      <input type="radio" name={id}/>
+    <span
+      onClick={select}
+    >
+      <input 
+        type="radio" 
+        name={id} 
+        onChange={() => console.log("selecterd")}
+        checked={on}
+        
+      />
       {children}
     </span>
   )
