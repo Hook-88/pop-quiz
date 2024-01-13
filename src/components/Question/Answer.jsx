94dcf58bd1f288ebc40c30f63ee17aa9f53219bd
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-01-12 23:32:06 -0500

    add logic to select an answer

--- src/components/Question/Answer.jsx
+++ src/components/Question/Answer.jsx
@@ -3,13 +3,15 @@ import { QuestionContext } from "./Question"
 import { QuizContext } from "../Quiz"
 import classNames from "classnames"
 
-//TODO make click on span second time posible
-//Idea: create state for question array per quesion obj en checked prop
 export default function Answer({children}) {
   const {id} = useContext(QuestionContext).questionData
   const {onUserInput, userInput} = useContext(QuizContext)
 
-  const cssSpan = classNames("answer-button")
+  const cssSpan = 
+    classNames(
+      "answer-button",  
+      {"checked": userInput[id] === children}
+    )
 
   return (
     <span
